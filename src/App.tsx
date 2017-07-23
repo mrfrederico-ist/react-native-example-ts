f7213c61adb2741bd3ebe1542aac6edb1db0a06b
Frederico Teixeira <mrfrederico89@gmail.com>
2017-07-23 20:43:46 +0100

    starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text } from 'react-native'
+import styled from 'styled-components/native'
 
-export default class App extends React.Component {
+class App extends React.Component {
   public render() {
     return (
-      <View style={styles.container}>
+      <Container>
         <Text>Open up App.ts to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      </Container>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-})
+// styles ===========
+const Container = styled.View`
+  align-items: center;
+  background-color: #fff;
+  flex: 1;
+  justify-content: center;
+`
+
+// ==================
+export default App
